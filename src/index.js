ebf373f4f687f9a98db810a9c12c6a07e7b9e739
Shine Ran <ranshine9@gmail.com>
2023-10-10 14:39:59 +0300

    tried redux devtools

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import store from "./customStore";
+import store from "./store";
 import * as actions from "./actions";
 
 store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded("Bug10"));
+store.dispatch(actions.bugAdded("Bug1"));
+store.dispatch(actions.bugAdded("Bug2"));
+store.dispatch(actions.bugAdded("Bug3"));
+store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
