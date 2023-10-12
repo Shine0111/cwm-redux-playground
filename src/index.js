b998cc2cccf4c474bfe84ded4d3d44bfb97a013e
Shine Ran <ranshine9@gmail.com>
2023-10-12 18:20:50 +0300

    created a selector function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from "./store/configureStore";
-import * as actions from "./store/bugs";
+import { bugAdded, bugResolved, getUnresolvedBugs } from "./store/bugs";
 import { projectAdded } from "./store/projects";
 
 const store = configureStore();
@@ -8,10 +8,11 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded({ description: "Bug1" }));
-store.dispatch(actions.bugAdded({ description: "Bug2" }));
-store.dispatch(actions.bugAdded({ description: "Bug3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+store.dispatch(bugAdded({ description: "Bug1" }));
+store.dispatch(bugAdded({ description: "Bug2" }));
+store.dispatch(bugAdded({ description: "Bug3" }));
+store.dispatch(bugResolved({ id: 1 }));
 store.dispatch(projectAdded({ name: "Project 1" }));
 
-console.log(store.getState());
+const unresolvedBug = getUnresolvedBugs(store.getState());
+console.log(unresolvedBug);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -24,3 +24,7 @@ const slice = createSlice({
 
 export const { bugAdded, bugResolved } = slice.actions;
 export default slice.reducer;
+
+// selector function
+export const getUnresolvedBugs = (state) =>
+  state.entities.bugs.filter((bug) => !bug.resolved);
