de81b04708f1a6d92cb37582aab2b893135f5377
Shine Ran <ranshine9@gmail.com>
2023-10-12 18:08:37 +0300

    combined reducers and added hierachy

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 // configureStore from redux-toolkit will allow us to talk to redux devTools automatically
 
-import reducer from "./projects";
+import reducer from "./reducer";
 
 export default function () {
   return configureStore({ reducer });
